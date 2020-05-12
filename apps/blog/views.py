2aa240b4e60c36a056c7c12e573ffcf1cf620783
JuanW18 <64981018+JuanW18@users.noreply.github.com>
2020-05-12 12:52:48 -0400

    viewsultima

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -26,32 +26,20 @@ class Index(View):
 class Programacion(ListView):
     model=Post
     template_name='programacion.html'
+    queryset=Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre__iexact='Programacion')   
+    )
     context_object_name='post'
-    
-    def get_queryset(self):
-        queryset = Post.objects.filter(
-            estado = True,
-            categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia'),
-        )
-        return queryset
-    
 
 class Tecnologia(ListView):
     model=Post
     template_name='tecnologia.html'
-    context_object_name='post'
-
-    def get_queryset(self):
-        queryset = Post.objects.filter(
+    queryset = Post.objects.filter(
             estado = True,
             categoria = Categoria.objects.get(nombre__iexact = 'Tecnologia'),
         )
-        return queryset
-
-
-
-    
-    
+    context_object_name='post'
 
 def post(request,slug):
     queryset=Post.objects.get(slug=slug)
